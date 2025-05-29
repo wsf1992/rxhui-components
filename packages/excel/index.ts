8657fbdf3726a2a1a05261779d66ce7d51c5027a
wsf1992 <741987899@qq.com>
2025-05-29 08:13:13 +0800

    fix: excel 组件引入 css

--- packages/excel/index.ts
+++ packages/excel/index.ts
@@ -2,16 +2,28 @@ import { LitElement, html, css, unsafeCSS } from 'lit'
 import { customElement, property, query } from 'lit/decorators.js'
 import Spreadsheet from "x-data-spreadsheet";
 import * as XLSX from 'xlsx';
-// 如何引入 css 文件
-// import { excelSheet } from './xspreadsheet.js'
-
-// console.log('xspreadcss', xspreadcss)
+// @ts-ignore
+import xspreadsheetStyles from 'x-data-spreadsheet/dist/xspreadsheet.css?inline'
 
 @customElement('rxhui-excel')
 export class RxhuiExcel extends LitElement {
-
     @property({ type: String, attribute: 'file-url' })
     accessor fileUrl = ''
+
+    static styles = css`
+        :host {
+            display: block;
+            width: 100%;
+            height: 100%;
+        }
+        
+        #x-spreadsheet {
+            width: 100%;
+            height: 100%;
+        }
+
+        ${unsafeCSS(xspreadsheetStyles)}
+    `;
 
     willUpdate(changedProperties: Map<string, any>) {
         console.log('changedProperties', changedProperties)
@@ -34,54 +46,60 @@ export class RxhuiExcel extends LitElement {
     @query('#x-spreadsheet')
     accessor xSpreadsheet!: HTMLElement
 
+
     private _fetchDatafromUrl = async () => {
-        console.log('_fetchDatafromUrl', this.fileUrl)
-        if (!this.fileUrl) return
-        // if(!this.xSpreadsheet) return setTimeout(() => this._fetchDatafromUrl(), 300)
+        // console.log('_fetchDatafromUrl', this.fileUrl)
+        // if (!this.fileUrl) return
+        // // if(!this.xSpreadsheet) return setTimeout(() => this._fetchDatafromUrl(), 300)
 
-        try {
-            const response = await fetch(this.fileUrl);
-            const arrayBuffer = await response.arrayBuffer();
-            const workbook = XLSX.read(arrayBuffer, { type: 'array' });
-            console.log('workbook', workbook)
+        // try {
+        //     const response = await fetch(this.fileUrl);
+        //     const arrayBuffer = await response.arrayBuffer();
+        //     const workbook = XLSX.read(arrayBuffer, { type: 'array' });
+        //     console.log('workbook', workbook)
 
-            // 转换为 x-data-spreadsheet 需要的格式
-            const data = workbook.SheetNames.map(sheetName => {
-                const worksheet = workbook.Sheets[sheetName];
-                const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1, raw: false });
-                return {
-                    name: sheetName,
-                    rows: jsonData.reduce((map, row, i) => {
-                        map[i] = {
-                            cells: row.reduce((colMap, column, j) => {
-                                colMap[j] = { text: column }
-                                return colMap
-                            }, {})
-                        }
-                        return map
-                    }, {})
-                };
-            });
-            console.log('转换为 x-data-spreadsheet 需要的格式', data, this.xSpreadsheet)
+        //     // 转换为 x-data-spreadsheet 需要的格式
+        //     const data = workbook.SheetNames.map(sheetName => {
+        //         const worksheet = workbook.Sheets[sheetName];
+        //         const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1, raw: false });
+        //         return {
+        //             name: sheetName,
+        //             rows: jsonData.reduce((map, row, i) => {
+        //                 map[i] = {
+        //                     cells: row.reduce((colMap, column, j) => {
+        //                         colMap[j] = { text: column }
+        //                         return colMap
+        //                     }, {})
+        //                 }
+        //                 return map
+        //             }, {})
+        //         };
+        //     });
+        //     console.log('转换为 x-data-spreadsheet 需要的格式', data, this.xSpreadsheet)
 
             const s = new Spreadsheet(this.xSpreadsheet, { mode: 'edit'})
-                .loadData({ sheets: data })
+                .loadData({ })
+                // .loadData({ sheets: data })
                 .change(data => {
                     console.log('spreadsheet changed:', data);
                 });
-            // s.validate()
-        } catch (error) {
-            console.error('Error loading Excel file:', error);
-        }
+        // } catch (error) {
+        //     console.error('Error loading Excel file:', error);
+        // }
     }
+
+    async firstUpdated() {
+        await this._fetchDatafromUrl();
+    }
+
+    // 初始化后，调用方法  this._fetchDatafromUrl()
+    
 
     render() {
         return html`
             <div id="x-spreadsheet"></div>
         `
     }
-
-    // static styles = [excelSheet]
 
 }
 
